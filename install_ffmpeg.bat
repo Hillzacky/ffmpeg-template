0be344732a464b12c30fffcf518dbf55ed023638
Hilmansyah <Hilm4nsyah@gmail.com>
2023-09-28 13:12:24 +0700

    Update install_ffmpeg.bat

--- install_ffmpeg.bat
+++ install_ffmpeg.bat
@@ -2,17 +2,21 @@
 setlocal
 
 set "ffmpegURL=https://www.ffmpeg.org/releases/ffmpeg-latest-win64-static.zip"
+set "ytdlURL=https://github.com/ytdl-org/youtube-dl/releases/download/2021.12.17/youtube-dl.exe"
 set "downloadPath=%~dp0"
-set "extractPath=%~dp0ffmpeg"
+set "sourcePath=%~dp0source"
 
 echo Downloading FFmpeg...
 curl -L -o "%downloadPath%ffmpeg.zip" "%ffmpegURL%"
 
 echo Extracting FFmpeg...
 mkdir "%extractPath%"
-tar -xzf "%downloadPath%ffmpeg.zip" -C "%extractPath%" --strip-components 1
+tar -xzf "%downloadPath%ffmpeg.zip" -C "%sourcePath%" --strip-components 1
+
+echo Downloading ytdl...
+curl -L -o "%downloadPath%\source\bin\youtube-dl.exe" "%ytdlURL%"
 
 echo Adding FFmpeg to PATH...
-setx PATH "%PATH%;%extractPath%\bin"
+setx PATH "%PATH%;%sourcePath%\bin"
 
-echo FFmpeg installation complete.
+echo Installation complete.
